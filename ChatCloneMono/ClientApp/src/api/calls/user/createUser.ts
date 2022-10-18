222226020d39ff5754a5c7c38a3f4949423a25f0
dSandley20 <danielsandley2@gmail.com>
2022-10-17 20:23:36 -0400

    use navigate on create user

--- ChatCloneMono/ClientApp/src/api/calls/user/createUser.ts
+++ ChatCloneMono/ClientApp/src/api/calls/user/createUser.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../../context/UserContext";
 import { ICreateUser } from "../../../interfaces/user";
 import { setToken } from "../../Api";
@@ -18,12 +19,14 @@ const createUserRequest = async (data: ICreateUser) => {
 const useCreateUser = () => {
   const { setAuthStatus } = useUserContext();
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   return useMutation(createUserRequest, {
     onSuccess: (response) => {
       setToken(response.data);
       setAuthStatus(true);
       queryClient.invalidateQueries(["getUser"]);
+      navigate("/home");
     },
   });
 };
